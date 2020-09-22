72f334d2247b71c4311cce607cdc5af07b588beb
Karel <Karel4ever@gmail.com>
2020-09-22 22:53:42 +0200

    Fade App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.black,
+        scaffoldBackgroundColor: Colors.blue,
       ),
       home: HomeScreen(),
     );
