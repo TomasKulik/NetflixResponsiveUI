caea974b0eb46b6275cddc6b3b9fc3331bc55720
Karel <Karel4ever@gmail.com>
2020-09-22 22:42:18 +0200

    Custom App Bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+import '../assets.dart';
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+        horizontal: 24.0,
+      ),
+      color: Colors.orange,
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+            const SizedBox(width: 12.0),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _AppBarButton(
+                    title: 'TV Shows',
+                    onTap: () => print('TV Shows'),
+                  ),
+                  _AppBarButton(
+                    title: 'Movies',
+                    onTap: () => print('Movies'),
+                  ),
+                  _AppBarButton(
+                    title: 'My List',
+                    onTap: () => print('My List'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  final String title;
+  final Function onTap;
+
+  const _AppBarButton({
+    Key key,
+    @required this.title,
+    @required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
